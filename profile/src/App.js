6615909be5b07c3218714685c17920ea4f6ed32f
Naveen Jakhar <naveen.jakhar11602376@gmail.com>
2021-04-03 14:07:56 +0530

    chat with myself

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,6 +1,6 @@
 import Select from "./Component/Select";
 import {Route, Switch,BrowserRouter} from 'react-router-dom';
-import Chat from "./Component/mychat";
+
 
 import Profile from "./Component/Profile"
 
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <>
      <Switch>
-        <Route exact path="/" component={Chat}/>
+        <Route exact path="/" component={Select}/>
         <Route exact path="/:userid" component={Profile}/>
         
 
